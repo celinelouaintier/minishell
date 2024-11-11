f353d8afdc0a00a8c9e9870ea62fe1af3da6d74a
nferrad <nferrad@student.42.fr>
2024-11-11 22:16:38 +0100

    Plus d'inspi sur les commits gl hf

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -3,14 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clouaint <clouaint@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nferrad <nferrad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 15:41:56 by nferrad           #+#    #+#             */
-/*   Updated: 2024/11/11 20:29:14 by clouaint         ###   ########.fr       */
+/*   Updated: 2024/11/11 22:19:10 by nferrad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*addback(char *s1, char c)
+{
+	size_t	i;
+	char	*str;
+	size_t	len;
+
+	i = 0;
+	len = 2;
+	if (s1 != NULL)
+		len += ft_strlen(s1);
+	str = malloc(len * sizeof(char));
+	if (!str)
+		return (NULL);
+	if (s1 != NULL)
+	{
+		while (s1[i])
+		{
+			str[i] = s1[i];
+			i++;
+		}
+		// free(s1);
+		// s1 = NULL;
+	}
+	str[i] = c;
+	str[i + 1] = '\0';
+	return (str);
+}
 
 void	print_token(t_token *token)
 {
@@ -74,13 +102,18 @@ char	*strarg(char *line, int *i, t_env *env)
 {
 	char	quote;
 	char	*arg;
-	char	*tmp;
-	char	*tmp2;
 
 	quote = 0;
 	arg = NULL;
 	while (line[*i])
 	{
+		if (line[*i] == quote)
+		{
+			quote = 0;
+			(*i)++;
+		}
+		if (!quote && !end_check(line[*i]))
+			break ;
 		if (!quote && (line[*i] == '\'' || line[*i] == '\"'))
 		{
 			quote = line[*i];
@@ -91,23 +124,8 @@ char	*strarg(char *line, int *i, t_env *env)
 		if (line[*i] == '$' && quote != '\'')
 			arg = set_arg(arg, line, i, env);
 		else
-		{
-			tmp = ft_substr(line, *i, 1);
-			tmp2 = ft_strjoin(arg, tmp);
-			free(arg);
-			arg = tmp2;
-			free(tmp);
-		}
+			arg = addback(tmp, line[*i]);
 		(*i)++;
-		if (!line[*i])
-			break ;
-		if (line[*i] == quote)
-		{
-			quote = 0;
-			(*i)++;
-		}
-		if (!quote && !end_check(line[*i]))
-			break ;
 	}
 	return (arg);
 }
@@ -142,24 +160,22 @@ int	set_index(char *line, int *i, t_env *env, t_token **token)
 		cmd = 1;
 		lstadd_back(token, lstnew(ft_substr(line, *i, 1), PIPE));
 	}
-	else if (cmd || *token == NULL)
-	{
-		int	j;
-		j = *i;
-		while (line[*i] && end_check(line[*i]))
-			(*i)++;
-		printf("fin : %d\n", *i);
-		lstadd_back(token, lstnew(ft_substr(line, j, *i - j), CMD));
-		cmd = 0;
-	}
 	else
 	{
 		arg = strarg(line, i, env);
 		if (!arg)
 			return (0);
-		if (lstlast(*token)->index == HEREDOX || lstlast(*token)->index == APPEND || lstlast(*token)->index == TRUNC || lstlast(*token)->index == INPUT)
-			cmd = 1;
-		lstadd_back(token, lstnew(arg, ARG));
+		if (cmd || !(*token))
+		{
+			lstadd_back(token, lstnew(arg, CMD));
+			cmd = 0;
+		}
+		else
+		{
+			if (lstlast(*token)->index == HEREDOX || lstlast(*token)->index == APPEND || lstlast(*token)->index == TRUNC || lstlast(*token)->index == INPUT)
+				cmd = 1;
+			lstadd_back(token, lstnew(arg, ARG));
+		}
 	}
 	return (1);
 }
